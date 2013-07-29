2633491f821849f708a48b0b08658a600116d16c
Chad Reed <creed30@gatech.edu>
2013-07-29 12:17:06 -0400

    Added the Course dapter file for DB team

---
+++ src/main/java/CourseAdapter.java
@@ -0,0 +1,12 @@
+package gradebook.model;
+import java.util.ArrayList;
+/**
+ * This is the Course Adapter interface for the DB team
+ * It contains a list of Courses
+ * @author Chad Reed
+ * @version 1.0
+ */
+public class CourseAdapter {
+    public static final ArrayList<Course>
+    GATECH_COURSE_LIST = new ArrayList<Course>();
+}
